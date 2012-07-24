f2d7adbb927ee7010f0a6d49d0337d1ad6219c81
7LI <77svli@gmail.com>
2012-07-24 08:31:40 +0800

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JplayerPlayFlv::Application.config.secret_token = 'c9f1811199ded12e822eff6b91637e91691ce304ee33570bca9d6a358217850b82e2c6f5e62dc7bb978b2e5a369b8d9620db157c6613bfb1c8a660c8f0e6026f'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+JplayerPlayFlv::Application.routes.draw do
+  root :to => "home#index"
+end
